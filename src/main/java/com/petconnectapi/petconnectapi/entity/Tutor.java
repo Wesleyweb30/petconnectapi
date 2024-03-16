4e304dc1ff54bfc5e790a99acb78816a83648113
Fellipe Silva <fellipe.silva.git@gmail.com>
2024-03-16 13:29:49 -0300

    PET-001 Entidade criada

---
+++ src/main/java/com/petconnectapi/petconnectapi/entity/Tutor.java
@@ -0,0 +1,47 @@
+package com.petconnectapi.petconnectapi.entity;
+
+import java.time.LocalDate;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Entity
+@Table(name = "tutors")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Tutor {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = true, unique = true)
+    private String tutorName;
+
+    @Column(nullable = true, unique = true)
+    private String email;
+
+    @Column(nullable = true, unique = true)
+    private String cpf;
+
+    @Column(nullable = true)
+    private char sex;
+
+    @Column(nullable = true)
+    @JsonFormat(pattern = "dd/MM/yyyy")
+    private LocalDate dtNasc;
+    
+
+    
+}
